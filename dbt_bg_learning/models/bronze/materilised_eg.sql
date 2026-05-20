1226be7c06cb2f9d8d8d2e6a764024c7f546bcae
Gabriel <kudzaishemanyanya@gmail.com>
2026-05-20 19:03:11 +0200

    update on materialised view

--- dbt_bg_learning/models/bronze/materilised_eg.sql
+++ dbt_bg_learning/models/bronze/materilised_eg.sql
@@ -1,6 +1,6 @@
 {{
   config(
-    materialized='materialised_view'
+    materialized='materialized_view'
   )
 }}
 
@@ -9,6 +9,4 @@ SELECT
       cst_create_date AS OpeningDate,  
       Count(cst_id) AS CustomerNumber
 FROM {{ ref('bronze_customers') }}
-WHERE cst_create_date <= '2026-05-20'
 GROUP BY cst_create_date
---ORDER BY cst_id, cst_create_date ASC
